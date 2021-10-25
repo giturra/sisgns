f2bd0d6ff2a23d8eff0cafddc64fbf19239727fc
Gabriel Iturra <gabrieliturrab@ug.uchile.cl>
2021-10-25 16:14:10 -0300

    tensor to cuda x10

--- skipgram.py
+++ skipgram.py
@@ -19,15 +19,15 @@ class SkipGram(torch.nn.Module):
         # print(x_pos_1, x_pos_2)
         
         # log-likelihood w.r.t. x_pos
-        u = self.embedding_u(x_pos_1).to(self.device)
+        u = self.embedding_u(x_pos_1).to('cuda')
         
-        v = self.embedding_v(x_pos_2).to(self.device)
+        v = self.embedding_v(x_pos_2).to('cuda')
         x_pos = (u * v).sum(dim=1).view(1, -1)
         
         # print(x_pos)
         # log-likelihood w.r.t. x_neg
-        u = self.embedding_u(x_neg_1)
-        v = self.embedding_v(x_neg_2)
+        u = self.embedding_u(x_neg_1).to('cuda')
+        v = self.embedding_v(x_neg_2).to('cuda')
         x_neg = (u * v).sum(dim=2)
         # print(x_neg)
         x = torch.cat((x_pos, x_neg)).T
